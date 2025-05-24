1645c61856c7e1e7870c7b2b759c1d03f9452dd0
Kien Lam <lorine93skilesvca@hotmail.com>
2025-05-24 18:56:53 +0900

    edit 404

--- app/not-found/layout.tsx
+++ app/not-found/layout.tsx
@@ -1,29 +1,9 @@
 import type { ReactNode } from "react";
-import { Press_Start_2P } from "next/font/google";
-import { ThemeProvider } from "@/components/theme-provider";
-import ClientProviders from "@/components/clientProviders";
-
-const pressStart2P = Press_Start_2P({ weight: "400", subsets: ["latin"] });
 
 export default function NotFoundLayout({
   children,
 }: {
   children: ReactNode;
 }) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={pressStart2P.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="light"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <ClientProviders>
-            {children}
-          </ClientProviders>
-        </ThemeProvider>
-      </body>
-    </html>
-  );
+  return children;
 }
